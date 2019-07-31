82f8c8559423d25ebe7acb52b258931a7fee1bef
Luis Otavio <luizcanalgame@gmail.com>
2019-07-30 22:32:23 -0300

    Implementando a logica dos arquivos

--- src/Program/Program.java
+++ src/Program/Program.java
@@ -1,6 +1,11 @@
 package Program;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -15,20 +20,31 @@ public class Program {
 		
 		while(!"exit()".equals(cod)) {
 			switch (cod) {
-			case "coun":
-				try {
+			case "acount()":
+				System.out.print("Local onde quer armazena(c:\\exemplo\\titulo.txt):");
+				String arq = sc.nextLine();
+				try (BufferedWriter br = new BufferedWriter(new FileWriter(arq))) {
+					SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 					System.out.print("Nome:");
 					String nome = sc.nextLine();
+					
 					System.out.print("Email:");
 					String email = sc.nextLine();
+					
 					System.out.print("Senha:");
 					String senha = sc.nextLine();
-					System.out.print("Numero de telefone:");
-					int numero = sc.nextInt();
+					
+					Date data = new Date();
+					br.write(sdf.format(data)+"\nnome: "+nome+"\nemail: "+email+"\nsenha: "+senha);
 				}
 				catch(java.util.InputMismatchException e) {
 					System.out.println("use somente numeros");
 				}
+				
+				catch(IOException e) {
+					System.out.println("Erro: "+e.getMessage());
+				}
+
 				break;
 
 			default:
